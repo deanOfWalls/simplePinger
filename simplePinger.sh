fc90257cc268f7daec0c5f18ac92063a83edd317
deanOfWalls <deanofwalls@gmail.com>
2023-08-07 00:22:39 -0400

    Debugging and resetting loop timer to 10s to diagnose why simple-pinger isn't sending requests to itself

--- simplePinger.sh
+++ simplePinger.sh
@@ -8,15 +8,21 @@ endpoint_urls=(
 "https://crud-demo-7igb.onrender.com/readAll"
 )
 
+# Debug: print the contents of the endpoint_urls array
+echo "Endpoint URLs: ${endpoint_urls[@]}"
+
 while true; do
   echo "Starting a new iteration at $(date)" # Debug message
   for url in "${endpoint_urls[@]}"; do
+    # Debug: print the URL being processed
+    echo "Processing URL: $url"
+    
     # Send GET request using curl and display the HTTP status code
     http_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
     echo "HTTP Status Code for $url: $http_code"
     
     # Wait 10s to send the next request, reset to 800 when done
-    sleep 800
+    sleep 10
   done
   echo "Ending iteration at $(date)" # Debug message
 done
