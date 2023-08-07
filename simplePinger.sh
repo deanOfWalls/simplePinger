c5e0451f99d9a0d8255e9d5d7173977c2ef17006
deanOfWalls <deanofwalls@gmail.com>
2023-08-06 23:18:43 -0400

    updated endpoint

--- simplePinger.sh
+++ simplePinger.sh
@@ -5,16 +5,18 @@ python server.py &
 
 endpoint_urls=(
 "https://simple-pinger.onrender.com" # Pinger itself
-"http://crud-demo-7igb.onrender.com/readAll"
+"https://crud-demo-7igb.onrender.com/readAll"
 )
 
 while true; do
+  echo "Starting a new iteration at $(date)" # Debug message
   for url in "${endpoint_urls[@]}"; do
     # Send GET request using curl and display the HTTP status code
     http_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
     echo "HTTP Status Code for $url: $http_code"
     
-    # Wait 800s to send the next request
-    sleep 800
+    # Wait 10s to send the next request, reset to 800 when done
+    sleep 10
   done
+  echo "Ending iteration at $(date)" # Debug message
 done
